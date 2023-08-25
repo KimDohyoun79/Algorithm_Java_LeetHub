1c6da7129c6b322288b688b60e2710a1c57631d0
KimDohyoun79 <dogdo5726@gmail.com>
2023-08-25 15:44:00 +0900

    Time: 1 ms (49.89%), Space: 55.1 MB (88.34%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+            
+        int len = nums.length;
+        if( k > len )
+            k =  k % len; 
+        
+         if (len == k || len == 1)
+                        return ;
+
+        // 이동해야 하는 앞부분 숫자 복사
+        int[] tmp = Arrays.copyOf(nums, len - k);
+
+        // 이동해야 하는 뒷부분 숫자 앞으로 옮기기 
+        for(int i = 0; i < k; i++){
+            nums[i] = nums[len - k + i];
+        }
+
+        // 복사한 앞부분 숫자를 붙여주기
+        for(int i = k; i < len; i++){
+            nums[i] = tmp[i-k];
+        }
+    }
+}
