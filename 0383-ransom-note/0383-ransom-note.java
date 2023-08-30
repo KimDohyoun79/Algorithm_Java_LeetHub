90b0f157acb8d52a5a0f8f93cc98200ea28f5d04
KimDohyoun79 <dogdo5726@gmail.com>
2023-08-30 23:15:05 +0900

    Time: 14 ms (49.72%), Space: 43.9 MB (59.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,9 +3,11 @@ class Solution {
         
         Map<Character, Integer> map = new HashMap();
         
+        // magazine 문자 map에 초기화
         for(char c : magazine.toCharArray())
             map.put(c, map.getOrDefault(c, 0) + 1);
         
+        // ransomNote 문자 존재 및 개수 확인
         for(char c : ransomNote.toCharArray()){
             if(map.containsKey(c) && map.get(c) > 0)
                 map.put(c, map.get(c) - 1);
