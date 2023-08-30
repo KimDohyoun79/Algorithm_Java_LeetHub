d7af8ace788407de302bfc0cbcee8cef2bf0adad
KimDohyoun79 <dogdo5726@gmail.com>
2023-08-30 16:27:14 +0900

    Time: 2 ms (82.08%), Space: 43.7 MB (50.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,22 @@
-import java.util.Hashtable;
+
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        int len = nums.length;
-        int[] re = new int[2];
+        int len = nums.length; // nums의 길이
+        int[] re = new int[2]; // 결과값으로 반환할 배열
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         
         Loop1 :
-        for(int i = 0; i < nums.length; i++){
-            for (int j = i + 1; j < len; j++) {
-                if(nums[i] + nums[j] == target){
-                    re[0] = i;
-                    re[1] = j;
-                    break Loop1;
-                }
+        for(int i = 0; i < len; i++){
+            int n = target - nums[i];
+            if(map.containsKey(n)){
+                re[0] = i;
+                re[1] = map.get(n);
+                break Loop1;
             }
+            map.put(nums[i], i);
+        
         }
 
         return re;
