cc575cd717a7d9fb63e1fbae46d46668fd94bb9a
KimDohyoun79 <dogdo5726@gmail.com>
2023-08-30 20:50:11 +0900

    Time: 18 ms (20.65%), Space: 43.3 MB (67.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,19 +3,12 @@ class Solution {
         
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         
-        
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
+        for(int i = 0; i < s.length(); i++)
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         
         for(int i = 0; i < t.length(); i++){
-            if( map.containsKey(t.charAt(i))){
-                if (map.get(t.charAt(i)) > 0)
+            if( map.containsKey(t.charAt(i)) && map.get(t.charAt(i)) > 0)
                     map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
-                else
-                    return false;
-            }
             else
                 return false;
         }
