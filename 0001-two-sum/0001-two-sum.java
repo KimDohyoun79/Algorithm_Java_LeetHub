c1f006454dcdd3b08b364bbe0558c9ed07c36ad0
KimDohyoun79 <dogdo5726@gmail.com>
2023-08-30 12:32:43 +0900

    Time: 49 ms (29.77%), Space: 43.5 MB (89.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,20 @@
+import java.util.Hashtable;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int len = nums.length;
+         int[] re = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < len; j++) {
+                if(nums[i] + nums[j] == target){
+                    re[0] = i;
+                    re[1] = j;
+                }
+            }
+        }
+        
+        
+        return re;
+    }
+}
