cc4c207a9da2d41dffcf79781f74ce8a066845cf
KimDohyoun79 <dogdo5726@gmail.com>
2023-09-11 02:15:00 +0900

    Time: 1 ms (49.55%), Space: 43.3 MB (32.71%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -14,24 +14,33 @@
  * }
  */
 
+import java.util.*;
+
 class Solution {
-    int min = Integer.MAX_VALUE;
-    TreeNode preNode = null;
-    
+
+    List<Integer> list; // 정렬된 val를 저장하기 위한 List
+
     public int getMinimumDifference(TreeNode root) {
+        list = new ArrayList<>();
+        
+        // 중위 탐색 및 노드의 val값을 list 저장
         inOrderDfs(root);
+
+		// 정렬된 list에서 최소 절대차 구하기
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < list.size() - 1; i++){
+            min = Math.min(min, list.get(i+1) - list.get(i));
+        }
+
         return min;
     }
 
+	// 중위 탐색 DFS
     public void inOrderDfs(TreeNode node){
-
+        
         if(node != null){
             inOrderDfs(node.left);
-
-            if(preNode != null)
-                min = Math.min(min, node.val - preNode.val);
-            preNode = node;
-            
+            list.add(node.val);
             inOrderDfs(node.right);
         }
     }
